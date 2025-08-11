4e0b1d7c8fdfc0fe68121cd510c665645c5d7260
Federica Ninno <federicaninno@Mac.Starry-inc.net>
2025-08-11 14:58:50 -0400

    New comment for branching only

--- dummy_python_example.py
+++ dummy_python_example.py
@@ -12,3 +12,4 @@ if __name__ == "__main__":
     print(f"5 + 7 = {result}")
 
 # ADD comment to see whether it is preserved on GitHub
+# ADD a comment just to see whether I can visualze it on the branch created
